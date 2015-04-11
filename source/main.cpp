43e99f24fbb5996da06632ea8a35fc1ee11eb464
Ashik Ahmed <ashik.992@gmail.com>
2015-04-11 23:31:18 +0600

    Just experimenting

---
+++ source/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <GL/glut.h>
+
+using namespace std;
+
+static GLfloat spin = 0.0;
+static GLfloat spin_speed = 2.0;
+
+float spin_x = 1;
+float spin_y = 1;
+float spin_z = 1;
+
+void init();
+void myDisplay();
+void spinDisplay(void);
+
+void drawColorCube(int x, int y, int z, int s){
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex2i(x, y);
+    glVertex2i(x+s, y);
+    glVertex2i(x, y+s);
+    glVertex2i(x+s, y+s);
+    glEnd();
+
+    glColor4f(0.5, 0.5, 0.0, 1.0);
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex3i(x, y, z+s);
+    glVertex3i(x+s, y, z+s);
+    glVertex3i(x, y+s, z+s);
+    glVertex3i(x+s, y+s, z+s);
+    glEnd();
+
+    for (float i = z; i <= z+s; i+=0.05) {
+        glColor4f(i/s, (1-i)/s, 0.5, 1.0);
+        glBegin(GL_LINE_LOOP);
+        glVertex3f(x, y, i);
+        glVertex3f(x+s, y, i);
+        glVertex3f(x+s, y+s, i);
+        glVertex3f(x, y+s, i);
+        glEnd();
+    }
+
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(1000, 1000);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Rubik's cube");
+    init();
+    glutDisplayFunc(myDisplay);
+    glutIdleFunc(spinDisplay);
+    glutMainLoop();
+    glutSwapBuffers();
+
+    return 0;
+}
+
+void init(){
+    glClearColor(0, 0, 0, 0);
+    glShadeModel(GL_SMOOTH);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void myDisplay(void){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    glRotatef(spin, spin_x, spin_y, spin_z);
+    drawColorCube(-2, -2, -2, 2);
+
+    glColor4f(1.0, 1.0, 1.0, .5);
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex3i(2, 2, 2);
+    glVertex3i(2, -2, 2);
+    glVertex3i(-2, -2, 2);
+    glVertex3i(-2, 2, 2);
+    glEnd();
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+void spinDisplay(void){
+
+    spin = 20;
+
+    if (spin>360.0) {
+
+        spin -= 360.0;
+    }
+    glutPostRedisplay();
+}
