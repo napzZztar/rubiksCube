d29d943ac1f91a1f7959aa4e02b004f79bd0e016
Ashik Ahmed <ashik.992@gmail.com>
2015-04-15 23:24:16 +0600

    Plane 5 & 6

--- source/part.h
+++ source/part.h
@@ -37,7 +37,6 @@ class part{
     void plot(){
         if(plane==1 || plane==3){
             float plane_z = 1.5; //initially for plane 1
-
             red;
 
             if (plane == 3){ //if it's the orange side
@@ -66,6 +65,21 @@ class part{
             glVertex3f(plane_x, upper_x, lower_y);
             glVertex3f(plane_x, upper_x, upper_y);
             glVertex3f(plane_x, lower_x, upper_y);
+            glEnd();
+        }else if (plane==5 || plane==6) {
+            float plane_y = 1.5;
+            blue;
+            
+            if (plane == 6) {
+                green;
+                plane_y = -1.5;
+            }
+
+            glBegin(GL_POLYGON);
+            glVertex3f(lower_x, plane_y, lower_y);
+            glVertex3f(upper_x, plane_y, lower_y);
+            glVertex3f(upper_x, plane_y, upper_y);
+            glVertex3f(lower_x, plane_y, upper_y);
             glEnd();
         }
     }
